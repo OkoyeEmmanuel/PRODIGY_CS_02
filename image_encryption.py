1940e0571aab04eb0add0bbad4443fd699e6760f
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-08-28 23:53:45 +0200

    Update image_encryption.py
    Created the Window, with the button. The button right now only shows the word "Image"

--- image_encryption.py
+++ image_encryption.py
@@ -4,3 +4,13 @@ from tkinter import filedialog
 root = Tk()
 root.geometry("400x400") # the box size, GUI
 
+
+def encrypt_image(): 
+   print("Image")
+
+button = Button(root, text="Select to encrypt an image", command=encrypt_image)
+button.place(x=100, y=100)
+
+
+
+root.mainloop() 

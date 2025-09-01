bd8b93de4dc613ca2d7a0f9cf5216cee5d493edb
OkoyeEmmanuel <okoye.tochukwu.emmanuel@gmail.com>
2025-09-01 17:18:03 +0200

    Update image_encryption.py
    Decided to use the CLI rather than a GUI

--- image_encryption.py
+++ image_encryption.py
@@ -1,16 +1,49 @@
+from logging import root
 from tkinter import *
 from tkinter import filedialog
 
 root = Tk()
-root.geometry("400x400") # the box size, GUI
+root.title("Image Encryption/Decryption")
+root.geometry("400x200")
 
+def encrypt_image(key):
+    global filename
+    filename = filedialog.askopenfilename(initialdir="/", title="Select an image to encrypt", filetypes=(("png files","*.png"), ("jpeg files","*.jpeg"), ("all files","*.*")))
+    print(filename)
+root.destroy()
+    
 
-def encrypt_image(): 
-   print("Image")
+def decrypt_image(key):
+    global filename
+    filename = filedialog.askopenfilename(initialdir="/", title="Select an image to decrypt", filetypes=(("png files","*.png"), ("jpeg files","*.jpeg"), ("all files","*.*")))
+    print(filename)
+    root.destroy()
 
-button = Button(root, text="Select to encrypt an image", command=encrypt_image)
-button.place(x=100, y=100)
+bExit = False
 
+while not bExit:
+    print("=============================================")
+    print("Welcome to Image Encryption Program")
+    print("=============================================")
+    print("1. Encrypt an image")
+    print("2. Decrypt an image")
+    print("3. Exit")
+    try:
+        choice = int(input("Enter your choice: "))
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
 
+    if choice == 1:
+        encryption_key = int(input("Enter encryption key: "))
+        encrypt_image(encryption_key)
+    elif choice == 2:
+        decryption_key = int(input("Enter encryption key: "))
+        decrypt_image(decryption_key)
+    elif choice == 3:
+        bExit = True
+        print("Exiting the program...")
+    else:
+        print("Invalid choice. Please try again.")
+        
 
-root.mainloop() 
